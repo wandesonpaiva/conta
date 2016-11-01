b831c8fafda0b56a3094e742d3b3b9b133313167
Wandeson Paiva <wandeson.jpaiva@gmail.com>
2016-11-01 11:13:09 -0300

    subir classes de test do dominio

---
+++ src/test/java/br/edu/ifrn/sanguebom/dominio/EnderecoTests.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package br.edu.ifrn.sanguebom.dominio;
+
+//import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EnderecoTests {
+
+	@Test
+	public void compareTo() {
+		Set<Endereco> enderecos = new TreeSet<>();
+		
+		Endereco endereco1 = Endereco.builder().cidade("Natal").estado("RN").bairro("Potengi").builder();
+		enderecos.add(endereco1);
+		Endereco endereco2 = Endereco.builder().cidade("Parnamirim").estado("RN").bairro("Rosa dos ventos").builder();
+		enderecos.add(endereco2);
+
+		assertThat(enderecos.iterator().next()).isEqualTo(endereco1);
+	}
+
+}
